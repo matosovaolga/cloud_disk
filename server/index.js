6ffbd0cd5f556360558981a3851b620a601aa9df
Olga Matosova <matosovaolga@gmail.com>
2024-04-15 20:36:38 +0300

    vercel conf

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const path = require("path");
 
 app.use(fileUpload({}));
 app.use(corsMiddleware);
-app.use(filePathMiddleware(path.resolve(__dirname, 'files')));
+app.use(filePathMiddleware(path.resolve(__dirname, "files")));
 
 app.use(express.json());
 app.use(express.static("static"));
