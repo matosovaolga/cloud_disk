9927be6547f295806debde0072a2539a875be35d
Olga Matosova <matosovaolga@gmail.com>
2024-04-16 16:43:08 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -3,29 +3,30 @@ const mongoose = require("mongoose");
 const config = require("config");
 const fileUpload = require("express-fileupload");
 const authRouter = require("./routes/auth.routes");
-import cors from "cors";
+const cors = require("cors");
 const fileRouter = require("./routes/file.routes");
 // const PORT = process.env.PORT || config.get("serverPort");
+const corsOptions = {
+  origin: "",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Origin",
+    "X-Requested-With",
+    "Accept",
+  ],
+};
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 
 // const corsMiddleware = require("./middleware/cors.middleware");
 const filePathMiddleware = require("./middleware/filepath.middleware");
 
 const path = require("path");
 
-// const corsOptions = {
-//   origin: "cloud-disk-server.vercel.app",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   //   credentials: true,
-//   allowedHeaders: [
-//     "Content-Type",
-//     "Authorization",
-//     "Origin",
-//     "X-Requested-With",
-//     "Accept",
-//   ],
-// };
+
 
 app.use(fileUpload({}));
 // app.use(corsMiddleware());
@@ -34,6 +35,12 @@ app.use(filePathMiddleware(path.resolve(__dirname, "files")));
 
 app.use(express.json());
 app.use(express.static("static"));
+app.options("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.sendStatus(204);
+});
 app.use("/api/auth", authRouter);
 app.use("/api/files", fileRouter);
 
