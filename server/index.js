d66da9e73d5107e47d144d6ea25fefe7c7737267
Olga Matosova <matosovaolga@gmail.com>
2024-04-16 09:16:15 +0300

    vercel conf

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const fileUpload = require("express-fileupload");
+const authRouter = require("./routes/auth.routes");
+const fileRouter = require("./routes/file.routes");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
