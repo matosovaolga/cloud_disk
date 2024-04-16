06ddc0726bf8aa96002c984d188740dfc6192e8c
Olga Matosova <matosovaolga@gmail.com>
2024-04-16 09:09:50 +0300

    vercel conf

--- server/index.js
+++ server/index.js
@@ -1,35 +1,8 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const config = require("config");
-const fileUpload = require("express-fileupload");
-const authRouter = require("./routes/auth.routes");
-const fileRouter = require("./routes/file.routes");
 const app = express();
-const PORT = process.env.PORT || config.get("serverPort");
 
-const corsMiddleware = require("./middleware/cors.middleware");
-const filePathMiddleware = require("./middleware/filepath.middleware");
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-const path = require("path");
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.use(fileUpload({}));
-app.use(corsMiddleware);
-app.use(filePathMiddleware(path.resolve(__dirname, "files")));
-
-app.use(express.json());
-app.use(express.static("static"));
-app.use("/api/auth", authRouter);
-app.use("/api/files", fileRouter);
-
-const start = async () => {
-  try {
-    mongoose.connect(config.get("dbUrl"));
-    app.listen(PORT, () => {
-      console.log(`Server start on port ${PORT}`);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-start();
+module.exports = app;
