a6231ab2d9b4fedc0c114e8787e64b163cc86187
Olga Matosova <matosovaolga@gmail.com>
2024-04-18 23:13:58 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ var corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.get("/products/:id", cors(corsOptions), function (req, res, next) {
+app.get("/auth", cors(corsOptions), function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for only example.com." });
 });
 
