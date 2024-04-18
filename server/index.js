b9b860b12656430b042913a88c3464c9984309f5
Olga Matosova <matosovaolga@gmail.com>
2024-04-18 23:23:18 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -32,8 +32,8 @@ app.use(filePathMiddleware(path.resolve(__dirname, "files")));
 app.use(express.json());
 app.use(express.static("static"));
 
-app.use("/api/auth", cors(),authRouter);
-app.use("/api/files",cors(), fileRouter);
+app.use("/api/auth", cors(corsOptions), authRouter);
+app.use("/api/files", cors(corsOptions), fileRouter);
 
 const start = async () => {
   try {
