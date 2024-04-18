aafe2e240ff00c9de100a90446e62d0fc036880e
Olga Matosova <matosovaolga@gmail.com>
2024-04-18 23:17:29 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -1,16 +1,50 @@
-var express = require("express");
-var cors = require("cors");
-var app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const fileUpload = require("express-fileupload");
+const authRouter = require("./routes/auth.routes");
+const cors = require("cors");
+const fileRouter = require("./routes/file.routes");
+// const PORT = process.env.PORT || config.get("serverPort");
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+const app = express();
+app.use(cors(corsOptions));
 
-var corsOptions = {
-  origin: "https://cloud-disk-client-nu.vercel.app/login",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// const corsMiddleware = require("./middleware/cors.middleware");
+const filePathMiddleware = require("./middleware/filepath.middleware");
+
+const path = require("path");
+
+app.use(fileUpload({}));
+// app.use(corsMiddleware());
+
+app.use(filePathMiddleware(path.resolve(__dirname, "files")));
+
+app.use(express.json());
+app.use(express.static("static"));
+
+app.use("/api/auth", authRouter);
+app.use("/api/files", fileRouter);
+
+const start = async () => {
+  try {
+    mongoose.connect(config.get("dbUrl"));
+    app.listen(process.env.PORT, () => {
+      console.log(`Server start on port ${process.env.PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-app.get("/auth", cors(corsOptions), function (req, res, next) {
-  res.json({ msg: "This is CORS-enabled for only example.com." });
-});
+start();
 
-app.listen(3000, function () {
-  console.log("CORS-enabled web server listening on port 80");
-});
+module.exports = app;
